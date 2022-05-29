ab81cc335a2cba566b7349136bdc3fd555a77e50
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-29 17:50:04 +0300

    add update image method to update image in db

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,8 +1,40 @@
+
 from unicodedata import category
 from django.db import models
 import datetime as dt
 
 # Create your models here.
+# class Category(models.Model):
+#     image_category = models.CharField(max_length=30)
+    
+#     def save_category(self):
+#         self.save()
+    
+#     def delete_category(self):
+#         self.delete()    
+        
+#     def get_category_id(cls,id):
+#         category = Category.object.get(pk=id)
+#         return category
+    
+#     def __str__(self):
+#         return self.image_category
+    
+# class Location(models.Model):
+#     image_location = models.CharField(max_length=30)
+    
+#     def save_location(self):
+#         self.save()
+        
+#     def delete_location(self): 
+#         self.delete()
+        
+#     def get_location_id(cls,id):
+#         location = Location.object.get(pk=id)
+#         return location   
+    
+#     def __str__(self):
+#         return self.image_location        
 
 class Image(models.Model):
     img_name =models.CharField(max_length=40)
@@ -40,8 +72,18 @@ class Image(models.Model):
     
     @classmethod
     def search_by_category(cls, search_term):
-        image= cls.objects.filter(category__image_category__icontains=search_term)
-        return image_category
+        image= cls.objects.filter(category__icontains=search_term)
+        return image
+    
+    @classmethod
+    def update_image(cls,current_value,new_value):
+        fetched_object = Image.objects.filter(img_name=current_value).update(img_name=new_value)
+        return fetched_object
+    
+    
+
+        
+        
         
         
       

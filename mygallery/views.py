9ba84d922346f824b6052a44c1d1deef72157248
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-30 03:38:25 +0300

    add search-result view and GET request method to search/get image by category

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Image,Category,Location
 
 # Create your views here.
+
+
+def search_results(request):
+    if 'image' in request. GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request, 'search.html',{"message":message,"all_imag": searched_images})
+    else:
+        message = "You haven't searched for any term"
+        return render (request, 'search.html',{"message":message})

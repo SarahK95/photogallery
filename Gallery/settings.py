e970fde3cc0596f605017b70f44285801153e659
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-01 20:17:05 +0300

    set debug to true for migration in settings.py

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -89,7 +89,8 @@ WSGI_APPLICATION = 'Gallery.wsgi.application'
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', True)
 # development
 if config('MODE')=="dev":
    DATABASES = {
